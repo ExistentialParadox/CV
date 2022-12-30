9d5ef0b4540ad4a2291a156eb315151aa5b4c5c0
ExistentialParadox <110877098+ExistentialParadox@users.noreply.github.com>
2022-12-30 15:58:22 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ==================== typing animation ==================*/
+
+var typed = new Typed(".typing", {
+    strings:["Web Designer", "Web developer", "Autodidact", "Traveller"],
+    typeSpeed:80,
+    BackSpeed:50,
+    loop:true
+})     
